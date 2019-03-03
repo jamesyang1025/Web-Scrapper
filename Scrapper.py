9b5887eb6261e0f6fa58b52519c575bb135fa6c8
jamesyang1025 <jamesyang1025@gmail.com>
2019-03-03 01:20:29 -0600

    scraping

--- Scrapper.py
+++ Scrapper.py
@@ -3,15 +3,31 @@ import re
 from bs4 import BeautifulSoup
 import logging
 import lxml
+import json
 
 actors_dict = {}
 movies_dict = {}
 
-def start_page(url):
-    page = requests.get(url)
 
+def start_scrapping(start_actor):
+    scrape_actor(start_actor)
+
+    movies_list = actors_dict[start_actor]['movies']
+
+    for i in range(len(movies_list)):
+        scrape_movie(movies_list[i])
+
+
+
+
+
+
+def scrape_actor(actor_name):
+    url = "https://en.wikipedia.org/wiki/" + actor_name
+
+    page = requests.get(url)
     if page.status_code != 200:
-        logging.error('Cannot open the url link')
+        logging.error("" + url + "cannot be found")
         return
 
     soup = BeautifulSoup(page.content, 'lxml')
@@ -22,17 +38,63 @@ def start_page(url):
     movies = movies_table.find_all('a')
     movies_list = []
     for movie in reversed(movies):
-        movies_list.append(movie.get('title').split('(')[0])
-    actors_dict[name] = movies_list
+        movies_list.append(movie.get('title'))
+    actors_dict[name] = {"name": name, "age": age, "movies": movies_list}
 
-    print("name: " + name)
-    print("age: " + str(age))
-    print(movies_list)
 
-    return
+def scrape_movie(movie_name):
+    url = "https://en.wikipedia.org/wiki/" + movie_name
+
+    page = requests.get(url)
+    if page.status_code != 200:
+        logging.error("" + url + "cannot be found")
+        return
+
+    soup = BeautifulSoup(page.content, 'lxml')
+    name = soup.find(id='firstHeading').get_text()
+    info_table = soup.find('table', {'class': 'infobox vevent'})
+    if info_table is None:
+        logging.info("Cannot find info table for " + movie_name)
+        return
+    info = info_table.find_all('tr')
+
+    actors = None
+    box_office = ""
+    for row in info:
+        if row.find(string=re.compile("Box office")):
+            box_office = row.find('td').get_text().split('[')[0]
+
+        if row.find(string=re.compile("Starring")):
+            actors = row.find_all('a')
+
+    grossing = 0
+    if box_office == "":
+        logging.info("Cannot find box office for " + movie_name)
+        grossing = 0
+    else:
+        if "million" in box_office:
+            tmp = box_office.split('$')[1]
+
+            box_office = re.findall(r"[-+]?\d*\.\d+|\d+", box_office)[0]
+
+            grossing = float(box_office) * 1000000
+            grossing = int(grossing)
+
+    actors_list = []
+    for actor in actors:
+        actors_list.append(actor.get('title'))
+
+    movies_dict[movie_name] = {"name": name, "grossing": grossing, "starring": actors_list}
 
 
 if __name__ == '__main__':
     start_url = "https://en.wikipedia.org/wiki/Jennifer_Connelly"
-    start_page(start_url)
+    start_actor = "Jennifer Connelly"
+    start_scrapping(start_actor)
     print(actors_dict)
+    print(movies_dict)
+
+    with open("data.json", 'w') as file_out:
+        data = {"Actors": actors_dict, "Movies": movies_dict}
+        json.dump(data, file_out, indent=4)
+        logging.info("Writing to json file completed")
